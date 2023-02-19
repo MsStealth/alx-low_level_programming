376ee7a7606bb238380af832623d814d99cc6e1c
MsStealth <onyedikachi@gmail.com>
2023-02-18 22:52:44 -0800

    using write to add quotes fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <unstd.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
 /**
- * main - A task where we cant use print and puts
- * Return: 1
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+exit(1);
 }
