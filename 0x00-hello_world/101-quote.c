b88782061e8723bdcc1f578ffa4c533b65db422b
MsStealth <onyedikachi@gmail.com>
2023-02-18 22:44:01 -0800

    using write to add quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - A task where we cant use print and puts
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
