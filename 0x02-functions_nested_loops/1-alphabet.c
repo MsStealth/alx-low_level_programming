cac247520cbe6d2d9710163cc48ebc7f7625f83f
MsStealth <onyedikachi@gmail.com>
2023-02-23 04:04:51 -0800

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,9 @@
 
 void print_alphabet(void);
 {
-	char letter;
+	char i;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
 	_putchar('\n');
 }
